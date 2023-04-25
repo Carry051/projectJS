12e86c4f6ec1f0884f2f531fde12dd0655d192ef
Maksym Voron <maksym.voron@sensoneo.com>
2023-04-25 20:50:01 +0200

    first com

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 "use strict";
 
-const hello = "hello ";
+const money = prompt("Ваш бюджет на месяц? ", ""),
+      time = prompt( "Введите дату в формате YYYY-MM-DD: ", " ");
+
+const appData = {
+  budget: money,
+  timeData: time,
+  expenses:{},
+  optionalExpenses: null,
+  income: [],
+  savings: false,
+};
+
+const a1 = prompt( 'Введите обязательную статью расходов в этом месяце'),
+      a2 = prompt( 'Во сколько обойдется?'),
+      a3 = prompt( 'Введите обязательную статью расходов в этом месяце'),
+      a4 = prompt( 'Во сколько обойдется?');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget/30);
