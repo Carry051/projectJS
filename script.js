e497aad9ee910d8cd30eb63653776fdb2a06f494
Maksym Voron <maksym.voron@sensoneo.com>
2023-05-10 20:16:14 +0200

    add doWhile and While

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const money = prompt("Ваш бюджет на месяц? ", ""),
+const money = +prompt("Ваш бюджет на месяц? ", ""),
       time = prompt( "Введите дату в формате YYYY-MM-DD: ", " ");
 
 const appData = {
@@ -12,12 +12,84 @@ const appData = {
   savings: false,
 };
 
-const a1 = prompt( 'Введите обязательную статью расходов в этом месяце'),
-      a2 = prompt( 'Во сколько обойдется?'),
-      a3 = prompt( 'Введите обязательную статью расходов в этом месяце'),
-      a4 = prompt( 'Во сколько обойдется?');
+for ( let i = 0; i < 2; i++) {
+    const a = prompt('Введите обязательную статью расходов в этом месяце'),
+          b = prompt('Во сколько обойдется?');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+    if( (typeof(a)) === 'string' &&
+        (typeof(a)) != null &&
+        a.length < 50 &&
+        (typeof(b)) != null &&
+        a != '' &&
+        b!= '' ) {
 
-alert(appData.budget/30);
+          console.log('Done');
+          appData.expenses[a] = b;
+    } else {
+      console.log('bad result');
+      i--;
+    }
+
+};
+
+
+// WHILE result
+
+// let i = 0;
+// while(i < 2) {
+//   i++;
+//   const a = prompt('Введите обязательную статью расходов в этом месяце'),
+//   b = prompt('Во сколько обойдется?');
+
+//   if( (typeof(a)) === 'string' &&
+//   (typeof(a)) != null &&
+//   a.length < 50 &&
+//   (typeof(b)) != null &&
+//   a != '' &&
+//   b!= '' ) {
+
+//     console.log('Done');
+//     appData.expenses[a] = b;
+//         } else {
+//       console.log('bad result');
+//       i--;
+//     }
+// }
+
+
+// DO...WHILE result
+
+// do {
+//   i++;
+//     const a = prompt('Введите обязательную статью расходов в этом месяце'),
+//     b = prompt('Во сколько обойдется?');
+
+//     if( (typeof(a)) === 'string' &&
+//     (typeof(a)) != null &&
+//     a.length < 50 &&
+//     (typeof(b)) != null &&
+//     a != '' &&
+//     b!= '' ) {
+
+//       console.log('Done');
+//       appData.expenses[a] = b;
+//           } else {
+//         console.log('bad result');
+//         i--;
+//       }
+// } while (i < 2);
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert(`Daily budget: ${appData.moneyPerDay}`);
+
+if(appData.moneyPerDay < 100) {
+    console.log('Minimum level budget');
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Middle level budget');
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Big level budget");
+} else {
+    console.log('Error');
+}
+
